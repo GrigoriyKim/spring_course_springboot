d50911c3f9d20e3d7dc0566a24781665a5a7f3da
Gkim <Grigoriy.kim1@gmail.com>
2023-04-24 14:57:21 +0300

    Initial commit

---
+++ src/test/java/com/gkim/spring/springboot/spring_course_springboot/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkim.spring.springboot.spring_course_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
